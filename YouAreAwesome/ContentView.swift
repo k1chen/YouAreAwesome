254f2061fed48ab9e61da8e50168da7112ef5649
k1chen <chenaun@bc.edu>
2025-01-20 18:03:49 -0500

    Ch 1.5 edits

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,33 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I Am A Programmer!" //@State lets var become changeable, always add private after state so it becomes localized
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         VStack {
-            Spacer() //creates evenly distributed space if you add multiple between things
-            Image(systemName: "swift")
+            Spacer()
+            Image(systemName: imageName)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             Text(message)
                 .font(.title2)
                 .fontWeight(.ultraLight)
                 .foregroundStyle(.black)
             Spacer()
             HStack{
-                Button("Awesome") {
-                    message = "Awesome!"
+                Button("Press Me!") {
+                    let message1 = "You Are Awesome!"
+                    let message2 = "You Are Great!"
+                    let imageName1 = "sun.max.fill"
+                    let imageName2 = "hand.thumbsup"
+                    if message == message1 {
+                        message = message2
+                        imageName = imageName2
+                    }
+                    else {
+                        message = message1
+                        imageName = imageName1
+                    }
                 }
                 .buttonStyle(.borderedProminent)
                 .font(.title2)
                 .tint(.orange)
-                Button("Great"){
-                    message = "Great!"
-                }
-                .buttonStyle(.borderedProminent)
-                .font(.title2)
-                .tint(.orange)
+
             }
   
         }
