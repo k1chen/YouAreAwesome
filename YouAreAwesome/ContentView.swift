3fe20e6655c85cf00d5c4c7b72d26589a20b61b6
k1chen <chenaun@bc.edu>
2025-01-20 20:48:47 -0500

    Ch 1.7 changes: added own images

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,40 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
+    @State private var imageString = ""
     @State private var imageName = ""
     var body: some View {
         VStack {
             Spacer()
-            Image(systemName: imageName)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             Text(message)
                 .font(.title2)
-                .fontWeight(.ultraLight)
-                .foregroundStyle(.black)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             Spacer()
             HStack{
                 Button("Press Me!") {
                     let message1 = "You Are Awesome!"
                     let message2 = "You Are Great!"
-                    let imageName1 = "sun.max.fill"
-                    let imageName2 = "hand.thumbsup"
-//                    if message == message1 {
-//                        message = message2
-//                        imageName = imageName2
-//                    }
-//                    else {
-//                        message = message1
-//                        imageName = imageName1
-//                    }
+                    let imageString1 = "sun.max.fill"
+                    let imageString2 = "hand.thumbsup"
+                    let imageName1 = "image0"
+                    let imageName2 = "image1"
+
                     message = ( message == message1 ? message2 : message1  )
+                    imageString = ( imageString == imageString1 ? imageString2 : imageString1)
                     imageName = ( imageName == imageName1 ? imageName2 : imageName1)
                 }
                 .buttonStyle(.borderedProminent)
                 .font(.title2)
-                .tint(.orange)
-
             }
   
         }
